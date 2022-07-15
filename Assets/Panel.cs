5f00d5a2ee37ad09ea5f61c2573999195e15216d
ang15 <32431798+ang15@users.noreply.github.com>
2022-07-16 00:47:27 +0300

    Initial commit

---
+++ Assets/Panel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+
+    public void Mouse()
+    {
+        GameManager.Instance.mouse = true;
+    }
+}
